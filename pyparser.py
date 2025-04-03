d5c722469068aff078784beed30c392985bdc12c
trk0012 <tylerryan1128@gmail.com>
2025-04-03 13:02:25 -0400

    Pushed Some Stuff
    Initial Commit Of The Parser and ini testset.

---
+++ pyparser.py
@@ -0,0 +1,27 @@
+from pycparser import c_parser, c_ast
+
+#C code as a multi-line string
+c_code = """
+void foo() {printf('foo');}
+void bar() {printf('bar');}
+
+int main() {
+    void (*fp)();
+    int input = 0;
+
+    if (input == 0)
+        fp = foo;
+    else
+        fp = bar;
+
+    fp();
+    return 0;
+}
+"""
+
+#Parse the code string
+parser = c_parser.CParser()
+ast = parser.parse(c_code)
+
+#Print the AST
+ast.show()
